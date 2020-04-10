4dace05bbe5017e9fcdcb9a6c8657d6231a94978
laurent VOURIOT <lauriot16@gmail.com>
2020-04-10 15:58:36 +0200

    can now create,remove,add and check file in workspaces, TODO add references

--- sppas/bin/workspaces.py
+++ sppas/bin/workspaces.py
@@ -33,15 +33,31 @@
     bin.workspaces.py
     ~~~~~~~~~~~~~
 
-:author:    Laurent Vouriot
+:author:       Laurent Vouriot
 :organization: Laboratoire Parole et Langage, Aix-en-Provence, France
 :contact:      contact@sppas.org
 :license:      GPL, v3
 :copyright:    Copyright (C) 2011-2020  Brigitte Bigi
-:summary:      a script to manage workspaces
+:summary:      a script to use workspaces from terminal
 
-Exemples:
-        TODO
+Examples:
+
+Create or open a workspace :
+>>>  ./sppas/sppas/bin/workspaces.py -w myWorkspace
+
+if -w not specified the workspace will be 'blank' by default
+
+Adding a file to the workspace :
+>>> ./sppas/sppas/bin/workspaces.py -w myWorkspace -a ./sppas/samples/samples-fra/BX_track_0451.wav
+
+Checking a file :
+>>> ./sppas/sppas/bin/workspaces.py -w myWorkspace -cf ./sppas/samples/samples-fra/BX_track_0451.wav
+
+if you want to check/uncheck all the files use the argument --check_all / unscheck_all
+
+An "all-in-one" solution :
+>>> ./sppas/sppas/bin/workspaces.py -w myWorkspace -a ./sppas/samples/samples-fra/BX_track_0451.wav -cf ./sppas/samples/samples-fra/BX_track_0451.wav
+
 """
 
 import sys
@@ -53,8 +69,10 @@ SPPAS = os.path.dirname(os.path.dirname(os.path.dirname(PROGRAM)))
 WORKSPACES = SPPAS + "/workspaces"
 sys.path.append(SPPAS)
 
-from sppas import sg
-from sppas.src.files import FileData
+from sppas.src.config import sg
+from sppas.src.files import FileData, FilePath
+from sppas.src.files import States
+from sppas.src.files.fileexc import FileOSError
 
 if __name__ == "__main__":
 
@@ -70,55 +88,62 @@ if __name__ == "__main__":
                                         sg.__copyright__, sg.__contact__)
     )
 
-    # Add arguments for actions
-    # -------------------------
+    # Add arguments for input/output files
+    # ---------------------------------------
 
-    group_act = parser.add_argument_group('Actions')
+    group_io = parser.add_argument_group('Files')
 
-    # pourquoi pas faire une seule option si le ws n'existe pas on le crée à ce moment
-    group_act.add_argument(
-        "-c",
-        metavar="create",
-        help="Create a new workspace."
+    group_io.add_argument(
+        "-w",
+        metavar="workspace",
+        help="open or create a workspace."
     )
 
-    group_act.add_argument(
+    group_io.add_argument(
         "-r",
         metavar="remove",
         help="remove an existing workspace."
     )
 
-    group_act.add_argument(
-        "-o",
-        metavar="open",
-        help="open a existing workspace."
-    )
-
-    group_act.add_argument(
+    group_io.add_argument(
         "-a",
         metavar="add",
         help="add a file to a workspace."
     )
 
-    # peut-être changer le nom de la commande
-    group_act.add_argument(
+    group_io.add_argument(
         "-rf",
         metavar="remove_file",
         help="remove a file of a workspace."
     )
 
-    group_act.add_argument(
+    # Arguments for setting the state of a file
+    # -----------------------------------------
+
+    group_state = parser.add_argument_group('State')
+
+    group_state.add_argument(
+        "--check_all",
+        action="store_true",
+        help="check all the files of the workspace you're working on"
+    )
+
+    group_state.add_argument(
+        "--uncheck_all",
+        action="store_true",
+        help="uncheck all the files of the workspace you're working on"
+    )
+    group_state.add_argument(
         "-cf",
-        metavar="check",
+        metavar="check_file",
         help="check a file of a workspace."
     )
 
-    group_act.add_argument(
+    group_state.add_argument(
         "-uf",
-        metavar="uncheck",
+        metavar="uncheck_file",
         help="uncheck a file of a workspace."
     )
-
 
     # Force to print help if no argument is given then parse
     # ------------------------------------------------------
@@ -127,35 +152,83 @@ if __name__ == "__main__":
         sys.argv.append('-h')
 
     args = parser.parse_args()
-
-    print(args)
-
-    # verify if arguments are with the rights options
-    # -----------------------------------------------
-
     arguments = vars(args)
 
-    if args.c:
-        print("created and saved new workspace {} in directory {}".format(args.c, WORKSPACES))
-        # TODO : create ws
-        ws = FileData(args.c)
-        ws.save("{}/{}.wjson".format(WORKSPACES, args.c))
-    if args.r:
-        print("remove existing workspace {}".format(args.r))
-        # bash ?
-        # TODO : remove ws
-    if args.o:
-        print("open existing workspace {}").format(args.o)
-        # TODO : load ws
-    if args.a:
-        print("add a file to the ws {}".format(args.a))
-        # TODO : add file to ws
-    if args.cf:
-        print("check file {}".format(args.cf))
-        # TODO : check file
-    if args.uf:
-        print("uncheck file {}".format(args.uf))
-        # TODO : check file
+    # default workspace : blank
+    # -------------------------
+
+    ws = FileData("blank")
+    savefile = "{}/blank.wjson".format(WORKSPACES)
+
+    try:
+        # open workspace, create one if not exist
+        if args.w:
+            savefile = "{}/{}.wjson".format(WORKSPACES, args.w)
+
+            # workspace exits, loading it
+            if os.path.isfile(savefile):
+                ws = ws.load(savefile)
+
+            # else creating a new one
+            else:
+                print("creating new workspace")
+                ws = FileData(args.w)
+                ws.save(savefile)
+            print("working on : {} in directory {}".format(args.w, WORKSPACES))
+
+        # remove existing workspace
+        if args.r:
+            os.remove("{}/{}.wjson".format(WORKSPACES, args.r))
+            print("remove existing workspace :  {}".format(args.r))
+
+        # adding a file to a workspace
+        if args.a:
+            ws.add_file(args.a)
+            ws.save(savefile)
+            print("added the file : {} ".format(args.a))
+
+        # removing a file of a workspace
+        if args.rf:
+            ws.remove_file(args.rf)
+            ws.save(savefile)
+            print("removed the file : {} from the workspace : {}".format(args.rf, ws))
+
+        # check a file
+        if args.cf:
+            if not os.path.isfile(args.cf):
+                raise FileNotFoundError("ERROR : {} not found".format(args.cf))
+            if args.all:
+                ws.set_object_state(States().CHECKED,)
+            else:
+                ws.set_object_state(States().CHECKED, ws.get_object(args.cf))
+            ws.save(savefile)
+            print("checked file : {}".format(args.cf))
+
+        # uncheck a file
+        if args.uf:
+            if not os.path.isfile(args.uf):
+                raise FileNotFoundError("ERROR : {} not found".format(args.cf))
+            ws.set_object_state(States().UNUSED, ws.get_object(args.cf))
+            ws.save(savefile)
+            print("unchecked file : {}".format(args.uf))
+
+        if args.check_all:
+            ws.set_object_state(States().CHECKED)
+            ws.save(savefile)
+            print("checked all files")
+
+        if args.uncheck_all:
+            ws.set_object_state(States().UNUSED)
+            ws.save(savefile)
+            print("unchecked all files")
+
+    except FileNotFoundError as e:
+        print(e)
+    except FileOSError as e:
+        print(e)
+
+
+
 
 
 
