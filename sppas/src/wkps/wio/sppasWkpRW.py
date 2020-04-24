7a7a8283740e4eacbe04051f13a31affd660e3d7
laurent VOURIOT <lauriot16@gmail.com>
2020-04-24 20:04:09 +0200

    sppas/src/wkps/wio/sppasWkpRW.py: added methods read() and create_wkp_from_extension()

--- sppas/src/wkps/wio/sppasWkpRW.py
+++ sppas/src/wkps/wio/sppasWkpRW.py
@@ -33,3 +33,74 @@
     ~~~~~~~~~~~~~~~~~~~~~~~~
 
 """
+from collections import OrderedDict
+from .sppasWJSON import sppasWJSON
+from sppas.src.utils.makeunicode import u
+
+# ----------------------------------------------------------------------------
+
+
+class sppasWkpRW():
+    """
+
+        :author:       Laurent Vouriot
+        :organization: Laboratoire Parole et Langage, Aix-en-Provence, France
+        :contact:      contact@sppas.org
+        :license:      GPL, v3
+        :copyright:    Copyright (C) 2011-2020  Brigitte Bigi
+        :summary:      Object used to create the instance of reader/writer depending on the workspace
+        (sppas or annotationPro)
+        """
+
+    WORKSPACE_TYPES = OrderedDict()
+    WORKSPACE_TYPES[sppasWJSON().default_extension.lower()] = sppasWJSON
+
+    def __init__(self, filename):
+        """Create a workspace reader/writer
+
+        :param filename: (str)
+        """
+        self.__filename = u(filename)
+
+    # ------------------------------------------------------------------------
+
+    def read(self, filename):
+        """Read a workspace from a file
+
+        :param filename: (str)
+        :returns: sppasWkpRW reader-writer
+        """
+
+        try:
+            wkp = sppasWkpRW.create_wkp_from_extension(self.__filename)
+        except FileNotFoundError as e:
+            print(e)
+
+        return wkp
+
+    # ------------------------------------------------------------------------
+
+    @staticmethod
+    def create_wkp_from_extension(filename):
+        """ Return a workspace according to a filename
+
+        :param filename: (str)
+        :returns: sppasBaseWkpIO()
+
+        """
+        for filereader in sppasWkpRW.WORKSPACE_TYPES.values():
+            try:
+                if filereader.detect(filename):
+                    return filereader()
+            except:
+                continue
+        return
+
+    # ------------------------------------------------------------------------
+
+    def write(self, workspace):
+        """Write a workspace into a file
+
+        :param workspace: (workspace)
+
+        """
