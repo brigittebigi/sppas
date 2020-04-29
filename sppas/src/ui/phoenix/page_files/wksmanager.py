95975842862f32eb978946552e53375151a7aa31
laurent VOURIOT <lauriot16@gmail.com>
2020-04-29 11:25:00 +0200

    sppas/src/ui/phoenix/page_files/wksmanager.py: corrected issues

--- sppas/src/ui/phoenix/page_files/wksmanager.py
+++ sppas/src/ui/phoenix/page_files/wksmanager.py
@@ -43,6 +43,7 @@ from sppas.src.utils import u
 from sppas.src.wkps.sppasWorkspace import sppasWorkspace
 from sppas.src.wkps.filebase import States
 from sppas.src.wkps.sppasWkps import sppasWkps
+from sppas.src.wkps.wio.sppasWJSON import sppasWJSON
 
 
 from ..windows import Confirm, Error
@@ -134,7 +135,7 @@ class WorkspacesManager(sppasPanel):
             name=name)
 
         # The data this page is working on
-        self.__data = sppasWorkspace()
+        self.__data = sppasWJSON()  
 
         # Construct the panel
         self._create_content()
