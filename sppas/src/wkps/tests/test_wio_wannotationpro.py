def5e73e2a2a90abb8313af07b6ac98bdf3075e9
Barthelemy Drabczuk <37699208+Barthelemy-Drabczuk@users.noreply.github.com>
2020-05-06 14:41:06 +0200

    sppas/src/wkps/tests/test_wio_wannotationpro.py: changed some tests

--- sppas/src/wkps/tests/test_wio_wannotationpro.py
+++ sppas/src/wkps/tests/test_wio_wannotationpro.py
@@ -46,7 +46,7 @@ from sppas.src.wkps.filestructure import FilePath
 class TestsppasWANT(unittest.TestCase):
 
     def setUp(self):
-        self.antw = sppasWANT("jul")
+        self.antw = sppasWANT("ant")
 
     # -----------------------------------------------------------------------
 
@@ -62,58 +62,34 @@ class TestsppasWANT(unittest.TestCase):
 
     # -----------------------------------------------------------------------
 
-    def test_serialize(self):
-        jul = sppasWANT()
-        fp = FilePath("annotpro.ws")
-
-        sub = dict()
-        id_group = "IdGroup"
-        open_count = "OpenCount"
-        edit_count = "EditCount"
-        listen_count = "ListenCount"
-        accepted = "Accepted"
-
-        sub["id_group"] = id_group
-        sub["open_count"] = open_count
-        sub["listen_count"] = listen_count
-        sub["edit_count"] = edit_count
-        sub["accepted"] = accepted
-
-        fp.subjoined = sub
-        jul.add(fp)
-
-        root = ET.Element("WorkspaceDataSet")
-        root.set("xmlns", "http://tempuri.org/WorkspaceDataSet.xsd")
-
-        test = jul._serialize(root)
-
-        for elem in test.iter():
-            print(elem)
-
-    # -----------------------------------------------------------------------
-
     def test_read(self):
-        fp = FilePath(os.path.dirname("annprowkpa.ant"))
-        fpath = self.antw.read("./AnnotProWkp.antw")
+        fp = FilePath(os.path.dirname("AnnotProWkp.antw"))
+        self.antw.read("./AnnotProWkp.antw")
 
-        sub = ["b1b36c56-652c-4390-81ce-8eabd4b0260f", "00000000-0000-0000-0000-000000000000",
-               "0", "4", "5", "false"]
+        # AnnotProWkp contains theses informations
+        sub = {"{http://tempuri.org/WorkspaceDataSet.xsd}IdGroup": "00000000-0000-0000-0000-000000000000",
+               "{http://tempuri.org/WorkspaceDataSet.xsd}Name": "annprowkp.ant",
+               "{http://tempuri.org/WorkspaceDataSet.xsd}OpenCount": "0",
+               "{http://tempuri.org/WorkspaceDataSet.xsd}EditCount": "4",
+               "{http://tempuri.org/WorkspaceDataSet.xsd}ListenCount": "5",
+               "{http://tempuri.org/WorkspaceDataSet.xsd}Accepted": "false"}
 
-        # read return the right FilePath
-        self.assertEqual(fp, fpath)
+        identifier = "b1b36c56-652c-4390-81ce-8eabd4b0260f"
 
         # read fill the sppasWANT instance with the filepath
         self.assertTrue(fp in self.antw.get_all_files())
 
+        # verify the id of the workspace
+        self.assertEqual(self.antw.get_id(), identifier)
+
+        # fp.subjoined contains the right files
         for fp in self.antw.get_all_files():
-            self.assertTrue(type(fp.subjoined) is dict)
-            for value in fp.subjoined.values():
-                self.assertTrue(value in sub)
+            self.assertEqual(fp.subjoined, sub)
 
     # -----------------------------------------------------------------------
 
     def test_write(self):
-        fp = self.antw.read("./AnnotProWkp.antw")
+        fp = self.antw.read("./apWkp.antw")
         self.antw.write("./testapro.antw")
         fpath = self.antw.read("./testapro.antw")
         self.assertEqual(fp, fpath)
