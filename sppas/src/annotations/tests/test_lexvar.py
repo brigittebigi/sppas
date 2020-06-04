2771bb98c312e0f4b8be6fd81138cf836b99d2f5
laurent VOURIOT <lauriot16@gmail.com>
2020-06-04 10:29:03 +0200

    sppas/src/annotations/tests/test_lexvar.py : added some tests

--- sppas/src/annotations/tests/test_lexvar.py
+++ sppas/src/annotations/tests/test_lexvar.py
@@ -58,6 +58,7 @@ from ..SpkLexVar.sppaslexvar import sppasLexVar
 
 
 class test_sppaslexvar(unittest.TestCase):
+
     def setUp(self):
         self.lexvar = sppasLexVar()
 
@@ -100,74 +101,114 @@ class test_sppaslexvar(unittest.TestCase):
         annot1 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(1), sppasPoint(2))))
         annot2 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(2), sppasPoint(3))))
         annot3 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(3), sppasPoint(4))))
-
-        annot4 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(3), sppasPoint(5))))
-        annot5 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(3), sppasPoint(6))))
-        annot6 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(3), sppasPoint(7))))
+        annot4 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(4), sppasPoint(5))))
+        annot5 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(5), sppasPoint(6))))
+        annot6 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(6), sppasPoint(7))))
+        annot7 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(7), sppasPoint(8))))
+        annot8 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(8), sppasPoint(9))))
+        annot9 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(9), sppasPoint(10))))
+        annot10 = sppasAnnotation(sppasLocation(sppasInterval(sppasPoint(10), sppasPoint(11))))
 
         # tags
         tag1 = sppasTag("bonjour")
-        tag2 = sppasTag("bonsoir")
-        tag3 = sppasTag("@")
-        tag4 = sppasTag("#")
-        tag5 = sppasTag("é")
-        tag6 = sppasTag("è")
+        tag2 = sppasTag("ça")
+        tag3 = sppasTag("va")
+        tag4 = sppasTag("bien")
+        tag5 = sppasTag("toi")
 
-        tag7 = sppasTag(789)
-        tag8 = sppasTag(9)
-        tag9 = sppasTag(422)
-        tag10 = sppasTag(4)
-        tag11 = sppasTag(27)
+        tag6 = sppasTag("salut")
+        tag7 = sppasTag("oui")
+        tag8 = sppasTag("ça")
+        tag9 = sppasTag("va")
+        tag10 = sppasTag("bien")
 
         # labels
-        label1 = sppasLabel([tag1, tag2])
+        label1 = sppasLabel(tag1)
+        label2 = sppasLabel(tag2)
+        label3 = sppasLabel(tag3)
+        label4 = sppasLabel(tag4)
+        label5 = sppasLabel(tag5)
 
-        label2 = sppasLabel(tag3)
-        label2.append(tag4)
-
-        label3 = sppasLabel([tag5, tag6])
-
-        label4 = sppasLabel([tag7, tag8])
-
-        label5 = sppasLabel(tag9)
-        label6 = sppasLabel(tag10)
-        label7 = sppasLabel(tag11)
+        label6 = sppasLabel(tag6)
+        label7 = sppasLabel(tag7)
+        label8 = sppasLabel(tag8)
+        label9 = sppasLabel(tag9)
+        label10 = sppasLabel(tag10)
 
         annot1.append_label(label1)
         annot2.append_label(label2)
         annot3.append_label(label3)
-
-        self.tier_spk1.add(annot2)
-        self.tier_spk1.add(annot1)
-        self.tier_spk1.add(annot3)
-
         annot4.append_label(label4)
         annot5.append_label(label5)
+
+        self.tier_spk1.add(annot1)
+        self.tier_spk1.add(annot2)
+        self.tier_spk1.add(annot3)
+        self.tier_spk1.add(annot4)
+        self.tier_spk1.add(annot5)
+
         annot6.append_label(label6)
-        annot6.append_label(label7)
-        self.tier_spk2.add(annot4)
-        self.tier_spk2.add(annot5)
+        annot7.append_label(label7)
+        annot9.append_label(label8)
+        annot9.append_label(label9)
+        annot9.append_label(label10)
+
         self.tier_spk2.add(annot6)
+        self.tier_spk2.add(annot7)
+        self.tier_spk2.add(annot8)
+        self.tier_spk2.add(annot9)
+        self.tier_spk2.add(annot10)
 
         # browsing tests
         # --------------
 
-        for ann in self.tier_spk1:
-            print("\n")
-            for label in ann.get_labels():
-                print(type(label))
-                print(label)
-                for elem in label:
-                    print(type(elem[0]))
-                    label.set_score(elem[0], 10000)
-                    print(elem[0])
-                print(label)
-                print("\n")
-        print("\n")
-        for ann in self.tier_spk2:
-            print(ann.get_labels())
+        # for ann in self.tier_spk1:
+        #     for label in ann.get_labels():
+        #         for tag, score in label:
+        #             print(tag)
+        # print("\n")
+        # for ann1 in self.tier_spk2:
+        #     for label1 in ann1.get_labels():
+        #         for tag1, score in label1:
+        #             print(tag1)
 
     # -----------------------------------------------------------------------
 
-    def test_detect(self):
-        return
+    def test_contains(self):
+        list1 = ["a", "b", "c", "d", "e", "f"]
+        list2 = ["c", "d", "e"]
+        list3 = [1, 21, 23]
+        list4 = []
+        tuple1 = ("b", "c", "d")
+        list5 = [("a", "b"), ("c", "d"), ("e", "f")]
+        list6 = [("c", "d")]
+        list7 = ["a", "b", "c", "z"]
+
+        self.assertTrue(self.lexvar.contains(list1, list2))
+        self.assertFalse(self.lexvar.contains(list1, list3))
+        # /!\ an empty list is always contained in an other list
+        self.assertTrue(self.lexvar.contains(list1, list4))
+
+        self.assertTrue(self.lexvar.contains(list5, list6))
+
+        self.assertFalse(self.lexvar.contains(list1, tuple1))
+
+        self.assertFalse(self.lexvar.contains(list1, list7))
+
+    # -----------------------------------------------------------------------
+
+    def test_window(self):
+        sequence = ["a", "b", "c", "d", "e", "f"]
+        sub2 = [(1, 45), (45, 4), (78, 9)]
+        window_list = list()
+
+        for window in self.lexvar.window(sequence, 3):
+            window_list.append(window)
+        for sub in window_list:
+            self.assertTrue(self.lexvar.contains(sequence, list(sub)))
+            self.assertFalse(self.lexvar.contains(sequence, sub2))
+
+    # -----------------------------------------------------------------------
+
+    def test_lexical_variation_detect(self):
+        self.lexvar.lexical_variation_detect(self.tier_spk1, self.tier_spk2)
