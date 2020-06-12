f859829d23ebb41291768eaa9991c874f81aeda4
Floroux <florian.hocquet@etu.univ-amu.fr>
2020-06-12 21:13:58 +0200

    test_manageroutputs.py = adjust some tests

--- sppas/src/videodata/tests/test_manageroutputs.py
+++ sppas/src/videodata/tests/test_manageroutputs.py
@@ -45,108 +45,108 @@ class TestManagerOutputs(unittest.TestCase):
 
     def setUp(self):
         self.__mOptions = ManagerOptions("-person", usable=True, csv=True, video=True, folder=True)
-        self.__mOptions.set_options("portrait", "crop", "circle", 640, 480)
+        self.__mOptions.set_options("portrait", "crop", True, 640, 480)
         self.path = "../../../../../corpus/Test_01_Celia_Brigitte/montage_compressed.mp4"
         self.__mOutputs = ManagerOutputs(self.path, 25, self.__mOptions)
 
     # ---------------------------------------------------------------------------
 
     def test_path(self):
-        x, y = self.__mOutputs._ManagerOutputs__path_video(self.path)
+        x, y = self.__mOutputs._path_video(self.path)
         self.assertEqual(x, "C:\\Users\\Floroux\\Documents\\stage\\corpus\\Test_01_Celia_Brigitte\\")
         self.assertEqual(y, "montage_compressed")
 
     # ---------------------------------------------------------------------------
 
     def test_cfile_path(self):
-        path = self.__mOutputs._ManagerOutputs__cfile_path()
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_*" + self.__mOptions.get_pattern() + ".csv"
+        path = self.__mOutputs._cfile_path()
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_*" + self.__mOptions.get_pattern() + ".csv"
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__cfile_path(1)
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_1" + self.__mOptions.get_pattern() + ".csv"
+        path = self.__mOutputs._cfile_path(1)
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_1" + self.__mOptions.get_pattern() + ".csv"
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__cfile_path(500)
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_500" + self.__mOptions.get_pattern() + ".csv"
+        path = self.__mOutputs._cfile_path(500)
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_500" + self.__mOptions.get_pattern() + ".csv"
         self.assertEqual(path, path2)
 
     # ---------------------------------------------------------------------------
 
     def test_vfile_path(self):
-        path = self.__mOutputs._ManagerOutputs__vfile_path()
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_*" + self.__mOptions.get_pattern() + ".avi"
+        path = self.__mOutputs._vfile_path()
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_*" + self.__mOptions.get_pattern() + ".avi"
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__vfile_path(1)
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_1" + self.__mOptions.get_pattern() + ".avi"
+        path = self.__mOutputs._vfile_path(1)
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_1" + self.__mOptions.get_pattern() + ".avi"
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__vfile_path(500)
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_500" + self.__mOptions.get_pattern() + ".avi"
+        path = self.__mOutputs._vfile_path(500)
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_500" + self.__mOptions.get_pattern() + ".avi"
         self.assertEqual(path, path2)
 
     # ---------------------------------------------------------------------------
 
     def test_bfile_path(self):
-        path = self.__mOutputs._ManagerOutputs__base_path()
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_*" + self.__mOptions.get_pattern() + "_usable.avi"
+        path = self.__mOutputs._base_path()
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_*" + self.__mOptions.get_pattern() + "_usable.avi"
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__base_path(1)
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_1" + self.__mOptions.get_pattern() + "_usable.avi"
+        path = self.__mOutputs._base_path(1)
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_1" + self.__mOptions.get_pattern() + "_usable.avi"
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__base_path(500)
-        path2 = self.__mOutputs._ManagerOutputs__path + \
-                self.__mOutputs._ManagerOutputs__video_name + "_500" + self.__mOptions.get_pattern() + "_usable.avi"
+        path = self.__mOutputs._base_path(500)
+        path2 = self.__mOutputs._path + \
+                self.__mOutputs._video_name + "_500" + self.__mOptions.get_pattern() + "_usable.avi"
         self.assertEqual(path, path2)
 
     # ---------------------------------------------------------------------------
 
     def test_ffile_path(self):
-        path = self.__mOutputs._ManagerOutputs__ffile_path()
-        path2 = self.__mOutputs._ManagerOutputs__path + "*" + self.__mOptions.get_pattern() + os.path.sep
+        path = self.__mOutputs._ffile_path()
+        path2 = self.__mOutputs._path + "*" + self.__mOptions.get_pattern() + os.path.sep
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__ffile_path(1)
-        path2 = self.__mOutputs._ManagerOutputs__path + "1" + self.__mOptions.get_pattern() + os.path.sep
+        path = self.__mOutputs._ffile_path(1)
+        path2 = self.__mOutputs._path + "1" + self.__mOptions.get_pattern() + os.path.sep
         self.assertEqual(path, path2)
 
-        path = self.__mOutputs._ManagerOutputs__ffile_path(500)
-        path2 = self.__mOutputs._ManagerOutputs__path + "500" + self.__mOptions.get_pattern() + os.path.sep
+        path = self.__mOutputs._ffile_path(500)
+        path2 = self.__mOutputs._path + "500" + self.__mOptions.get_pattern() + os.path.sep
         self.assertEqual(path, path2)
 
     # ---------------------------------------------------------------------------
 
     def test_out_csv(self):
         with self.assertRaises(TypeError):
-            self.__mOutputs._ManagerOutputs__out_csv(["a"])
+            self.__mOutputs._out_csv(["a"])
         with self.assertRaises(ValueError):
-            self.__mOutputs._ManagerOutputs__out_csv("a")
+            self.__mOutputs._out_csv("a")
 
     # ---------------------------------------------------------------------------
 
     def test_out_video(self):
         with self.assertRaises(TypeError):
-            self.__mOutputs._ManagerOutputs__out_video(["a"])
+            self.__mOutputs._out_video(["a"])
         with self.assertRaises(ValueError):
-            self.__mOutputs._ManagerOutputs__out_csv("a")
+            self.__mOutputs._out_csv("a")
 
     # ---------------------------------------------------------------------------
 
     def test_out_folder(self):
         with self.assertRaises(TypeError):
-            self.__mOutputs._ManagerOutputs__out_folder(["a"])
+            self.__mOutputs._out_folder(["a"])
         with self.assertRaises(ValueError):
-            self.__mOutputs._ManagerOutputs__out_csv("a")
+            self.__mOutputs._out_csv("a")
 
     # ---------------------------------------------------------------------------
