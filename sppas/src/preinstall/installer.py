aa857b76209886ea1145432ed57e783c110a13d4
brigitte.bigi@gmail.com <brigitte.bigi@gmail.com>
2020-12-04 16:40:15 +0100

    installer: urllib import correction

--- sppas/src/preinstall/installer.py
+++ sppas/src/preinstall/installer.py
@@ -41,10 +41,10 @@ import subprocess
 import os
 try:
     # For Python 3.0 and later
-    from urllib.request import urlopen
+    import urllib
 except ImportError:
     # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+    import urllib2 as urllib
 import zipfile
 
 from sppas.src.config import cfg
